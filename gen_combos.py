71e28698cc94bda326b0088cfbe444d7c9d65e04
fecet <xiezej@gmail.com>
2022-03-19 17:43:39 +0800

    uppd

--- gen_combos.py
+++ gen_combos.py
@@ -2,7 +2,7 @@ import itertools
 
 cards = ["克拉斯", "纳贝尔", "人马", "天玑"]
 
-hands = list(itertools.combinations(cards, 2))
+hands = list(itertools.combinations(cards, 3))
 
 for hand in hands:
     print("+".join(hand))
